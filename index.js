48ea3186b458afb9e38381964d76f7aef6a05665
Anthony Cook <ddragonmaster49@yahoo.com>
2020-08-25 13:54:09 -0400

    Add client-side routing

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
+import 'regenerator-runtime/runtime'
+import {Router} from '@vaadin/router';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './js/pages/App.jsx';
+import App from './src/App.jsx';
+import SignupPage from './src/js/pages/Signup.jsx';
+import SigninPage from './src/js/pages/Signin.jsx';
 
-const node = document.getElementById("app");
-ReactDOM.render(<App/>, node)
+function isUserAuthorized() {
+  firebase.auth().currentUser ? true : false;
+}
+
+const routerMountPoint = document.getElementById("app");
+const reactContainer = document.getElementById("view");
+const emptyElement = document.createElement("div");
+
+const router = new Router(routerMountPoint);
+window.Router = Router;
+
+router.setRoutes([
+  {
+    path: '/',
+    children: [
+      {path: '', action: (context, commands) => {
+        if (!isUserAuthorized) Router.go('/signup');
+        ReactDOM.render(<App/>, reactContainer);
+        return emptyElement;
+      }},
+      {path: '/signup', action: (context, commands) => {
+        ReactDOM.render(<SignupPage router={router}/>, reactContainer);
+        return emptyElement;
+      }},
+      {path: '/signin', action: (context, commands) => {
+        ReactDOM.render(<SigninPage/>, reactContainer);
+        return emptyElement;
+      }},
+      {path: '(.*)', action: (context, commands) => {
+        const el = document.createElement("div");
+        el.textContent = "Error 404";
+        return el;
+      }}
+    ]
+  }
+])
