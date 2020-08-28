d9ff355b746f8d244ece1e5e8cddae64578453d1
Anthony Cook <ddragonmaster49@yahoo.com>
2020-08-28 12:25:54 -0400

    Replace routing solution

--- index.js
+++ index.js
@@ -1,44 +1,71 @@
 import 'regenerator-runtime/runtime'
-import {Router} from '@vaadin/router';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/App.jsx';
+import HomePage from './src/js/pages/Home.jsx';
 import SignupPage from './src/js/pages/Signup.jsx';
 import SigninPage from './src/js/pages/Signin.jsx';
+import EditNotePage from './src/js/pages/NoteEditor.jsx';
+import App from './src/js/pages/App.jsx';
+import UniversalRouter from 'universal-router';
 
 function isUserAuthorized() {
   firebase.auth().currentUser ? true : false;
 }
 
-const routerMountPoint = document.getElementById("app");
-const reactContainer = document.getElementById("view");
-const emptyElement = document.createElement("div");
-
-const router = new Router(routerMountPoint);
-window.Router = Router;
-
-router.setRoutes([
+const routes = [
   {
-    path: '/',
+    path: '',
     children: [
-      {path: '', action: (context, commands) => {
-        if (!isUserAuthorized) Router.go('/signup');
-        ReactDOM.render(<App/>, reactContainer);
-        return emptyElement;
-      }},
-      {path: '/signup', action: (context, commands) => {
-        ReactDOM.render(<SignupPage router={router}/>, reactContainer);
-        return emptyElement;
-      }},
-      {path: '/signin', action: (context, commands) => {
-        ReactDOM.render(<SigninPage/>, reactContainer);
-        return emptyElement;
-      }},
-      {path: '(.*)', action: (context, commands) => {
-        const el = document.createElement("div");
-        el.textContent = "Error 404";
-        return el;
-      }}
+      {
+        path: '',
+        action: async (context, params) => {
+          const childPage = await context.next();
+          if (!childPage) return;
+          if (!isUserAuthorized) return ({ redirect: '/signup' });
+          return { element: <App child={<div>{childPage.element}</div>} /> };
+        },
+        children: [
+          {
+            path: '',
+            action: () => {
+              return { element: <HomePage /> };
+            }
+          },
+          {
+            path: '/note/:id',
+            action: (context, params) => {
+              return { element: <EditNotePage noteId={params.id} /> };
+            }
+          }
+        ]
+      },
     ]
+  },
+  {
+    path: '/signup',
+    action: (context) => {
+      return { element: <SignupPage router={context.router} /> };
+    }
+  },
+  {
+    path: '/signin',
+    action: () => {
+      return ({ element: <SigninPage router={null} /> });
+    }
+  },
+  {
+    path: '(.*)',
+    action: () => {
+      return { element: <div>Error 404</div> };
+    }
   }
-])
+];
+
+const reactContainer = document.getElementById("view");
+const router = new UniversalRouter(routes)
+
+router.resolve(location.pathname)
+  .then(page => {
+    if (page.redirect) return window.location = (page.redirect)
+    ReactDOM.render(page.element, reactContainer);
+  })
