1b59fa822e040a1aed68dcaca133e75e2fafc5f2
Anthony Cook <ddragonmaster49@yahoo.com>
2020-09-09 19:43:59 -0400

    Replace universal-router with react-router

--- index.js
+++ index.js
@@ -6,66 +6,24 @@ import SignupPage from './src/js/pages/Signup.jsx';
 import SigninPage from './src/js/pages/Signin.jsx';
 import EditNotePage from './src/js/pages/NoteEditor.jsx';
 import App from './src/js/pages/App.jsx';
-import UniversalRouter from 'universal-router';
+import { Router, Route, IndexRoute, hashHistory} from 'react-router';
 
 function isUserAuthorized() {
   firebase.auth().currentUser ? true : false;
 }
 
-const routes = [
-  {
-    path: '',
-    children: [
-      {
-        path: '',
-        action: async (context, params) => {
-          const childPage = await context.next();
-          if (!childPage) return;
-          if (!isUserAuthorized) return ({ redirect: '/signup' });
-          return { element: <App child={<div>{childPage.element}</div>} /> };
-        },
-        children: [
-          {
-            path: '',
-            action: () => {
-              return { element: <HomePage /> };
-            }
-          },
-          {
-            path: '/note/:id',
-            action: (context, params) => {
-              return { element: <EditNotePage noteId={params.id} /> };
-            }
-          }
-        ]
-      },
-    ]
-  },
-  {
-    path: '/signup',
-    action: (context) => {
-      return { element: <SignupPage router={context.router} /> };
-    }
-  },
-  {
-    path: '/signin',
-    action: () => {
-      return ({ element: <SigninPage router={null} /> });
-    }
-  },
-  {
-    path: '(.*)',
-    action: () => {
-      return { element: <div>Error 404</div> };
-    }
-  }
-];
+function Notebook() {
+  return (
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={HomePage}/>
+        <Route path="note/:id" component={EditNotePage}/>
+      </Route>
+      <Route path="signup" component={SignupPage}/>
+      <Route path="signin" component={SigninPage}/>
+    </Router>
+  )
+}
 
 const reactContainer = document.getElementById("view");
-const router = new UniversalRouter(routes)
-
-router.resolve(location.pathname)
-  .then(page => {
-    if (page.redirect) return window.location = (page.redirect)
-    ReactDOM.render(page.element, reactContainer);
-  })
+ReactDOM.render(<Notebook/>, reactContainer)
