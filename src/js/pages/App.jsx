dfe189cf32f6499ee0fe8242d60dc63bbf8f2108
Anthony Cook <ddragonmaster49@yahoo.com>
2020-09-09 20:18:08 -0400

    Apply fade animation to all pages except signup & signin

--- src/js/pages/App.jsx
+++ src/js/pages/App.jsx
@@ -1,23 +1,24 @@
 import React, { useEffect } from "react";
 import useNoteStore from "../stores/useNoteStore.jsx";
 import NoteContext from "./../context/note-context";
+import Fade from "../components/Fade.jsx";
 
 const mockData = [{
-  id: 1, 
+  id: 1,
   title: 'Gym Schedule',
   dateCreated: Number(new Date()),
   text: 'Before premiere on Tuesday, buy a dress',
   tag: "Clothing",
   selected: false
 }, {
-  id: 2, 
+  id: 2,
   title: 'Exercise Goals',
   dateCreated: Number(new Date()),
   text: 'Run 2 miles every morning next week',
   tag: "Exercise",
   selected: false
-},{
-  id: 3, 
+}, {
+  id: 3,
   title: 'PHI 105',
   dateCreated: Number(new Date()),
   text: 'Finish homework for class on Monday',
@@ -25,16 +26,21 @@ const mockData = [{
   selected: false
 }];
 
-function App({child}) {
-  const {notes, setNotes, addNote, editNote, deleteNote, selectNote, save, load} = useNoteStore(mockData);
-  
+function App({ children, location }) {
+  const { notes, setNotes, addNote, editNote, deleteNote, selectNote, save, load } = useNoteStore(mockData);
+
   useEffect(() => {
     // load data from database
   }, []);
-  
-  return(
-    <NoteContext.Provider value={{notes, setNotes, addNote, editNote, deleteNote, selectNote, save, load}}> 
-      {child}
+
+  return (
+    <NoteContext.Provider value={{ notes, setNotes, addNote, editNote, deleteNote, selectNote, save, load }}>
+      <Fade childKey={location.pathname} 
+             enter={1000} 
+             exit={1000} 
+             classes={"my-node"}>
+        { children }
+      </Fade>
     </NoteContext.Provider>
   );
 }
