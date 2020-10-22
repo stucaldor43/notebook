4321c8ea707f1890556cf724dbe84fa2b0453bc7
Anthony Cook <ddragonmaster49@yahoo.com>
2020-10-21 20:04:18 -0400

    Fix issue where async tests would sometimes fail due to default timeout length used by waitFor utilities

--- setupTests.js
+++ setupTests.js
@@ -1,2 +1,8 @@
 import 'regenerator-runtime/runtime';
 import "@testing-library/jest-dom";
+import { configure } from '@testing-library/dom'
+import "jest-localstorage-mock";
+
+configure({
+  asyncUtilTimeout: 3000
+});
