f9145e4b747c31cd3f87002b181cc030d330e78a
Anthony Cook <ddragonmaster49@yahoo.com>
2020-10-21 20:08:15 -0400

    Fix issue where tests would fail due to error related to interactions between jest and firebase

---
+++ __test-utils__/custom-jest-environment.js
@@ -0,0 +1,25 @@
+// Workaround for error from jest internals as referenced here: https://github.com/facebook/jest/issues/6248
+"use strict";
+
+const BrowserEnvironment = require("jest-environment-jsdom");
+
+class MyEnvironment extends BrowserEnvironment {
+  constructor(config) {
+    super(
+      Object.assign({}, config, {
+        globals: Object.assign({}, config.globals, {
+          Uint32Array: Uint32Array,
+          Uint8Array: Uint8Array,
+          ArrayBuffer: ArrayBuffer,
+        }),
+      }),
+    );
+  }
+
+  async setup() {}
+
+  async teardown() {}
+
+}
+
+module.exports = MyEnvironment;
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|less)$": "identity-obj-proxy"
   },
-  setupFilesAfterEnv: ["<rootDir>/setupTests.js"]
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  testEnvironment: "./__test-utils__/custom-jest-environment.js"
 }
