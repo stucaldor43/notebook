029b60bc74b5483a7da643110f688fafedc37d03
Anthony Cook <ddragonmaster49@yahoo.com>
2020-12-06 17:10:08 -0500

    Remove references to NoteContext

--- src/js/components/AddNoteDialog.jsx
+++ src/js/components/AddNoteDialog.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Modal from "./Modal.jsx";
 import Button from "./Button.jsx";
-import NoteContext from "./../context/note-context";
 import "./../../css/components/AddNoteDialog/styles.css";
+import notesAPI from "./../api/notes";
+import generateUUID from '../utils/generateUUID.js';
 
 let dialogElement;
 
@@ -16,6 +17,19 @@ function AddNoteDialog({ closeDialog }) {
     }
   };
 
+  const addNote = async (title, text, tags) => {
+    const note = {
+      id: generateUUID(),
+      title,
+      dateCreated: firebase.firestore.Timestamp.fromDate(new Date()),
+      text: text || "",
+      tags: Array.isArray(tags) ? tags : [tags],
+      selected: false
+    };
+    await notesAPI().createNote(note);
+    return note;
+  }
+
   useEffect(() => {
     document.body.addEventListener("click", handleClick);
 
@@ -25,28 +39,28 @@ function AddNoteDialog({ closeDialog }) {
   }, [])
 
   return (
-    <NoteContext.Consumer>
-      {
-        ({ addNote }) => {
-          return (
-            <Modal classes="add-note-dialog">
-              <div ref={(el) => dialogElement = el} className="add-note-dialog__container">
-                <div className="add-note-dialog_header">
-                  <h2 className="add-note-dialog__title">Add Note</h2>
-                </div>
-                <div className="add-note-dialog__content">
-                  <input className="add-note-dialog__input" placeholder="Title of note" onChange={(e) => setTitle(e.target.value)} value={title}/>
-                </div>
-                <div className="add-note-dialog__footer">
-                  <Button onClick={closeDialog} classes="add-note-dialog__button add-note-dialog_button--cancel">Cancel</Button>
-                  <Button onClick={() => { addNote(title, "", []); setTitle(""); closeDialog() }} classes="add-note-dialog__button add-note-dialog_button--create">Create</Button>
-                </div>
-              </div>
-            </Modal>
-          );
-        }
-      }
-    </NoteContext.Consumer>
+    <Modal classes="add-note-dialog">
+      <div ref={(el) => dialogElement = el} className="add-note-dialog__container">
+        <div className="add-note-dialog_header">
+          <h2 className="add-note-dialog__title">Add Note</h2>
+        </div>
+        <div className="add-note-dialog__content">
+          <input className="add-note-dialog__input" placeholder="Title of note" onChange={(e) => setTitle(e.target.value)} value={title}/>
+        </div>
+        <div className="add-note-dialog__footer">
+          <Button onClick={closeDialog} classes="add-note-dialog__button add-note-dialog_button--cancel">Cancel</Button>
+          <Button onClick={async () => { 
+            const note = await addNote(title, "", []); 
+            setTitle(""); 
+            closeDialog();
+            location.assign(`/#/note/${note.id}`)
+           }} 
+            classes="add-note-dialog__button add-note-dialog_button--create">
+              Create
+          </Button>
+        </div>
+      </div>
+    </Modal>
   );
 }
 
