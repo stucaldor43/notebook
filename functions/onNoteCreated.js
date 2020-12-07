ee5256e60ad659c57003752ab1d82fbf76c5aa62
Anthony Cook <ddragonmaster49@yahoo.com>
2020-12-07 18:59:38 -0500

    Modify onNoteCreated function to see if indexing is working properly

--- functions/onNoteCreated.js
+++ functions/onNoteCreated.js
@@ -1,6 +1,16 @@
+import algoliasearch from 'algoliasearch';
+const client = algoliasearch(process.env.ALGOLIA_ID, process.env.ALGOLIA_ADMIN_KEY);
+
 exports.handler = async function (event, context) {
+  const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME);
+  index.saveObject({
+    objectID: "4kaj",
+    title: "First Post",
+    body: "Some text"
+  });
+
   return {
     statusCode: 200,
-    body: process.env.ALGOLIA_ID
+    body: "Success"
   };
 }
